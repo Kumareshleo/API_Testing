7cefbb7d96fc5ea658f57a62f07d353a76dfe349
Kumaresh R <Kumaresh R@192.168.0.102>
2020-05-06 17:18:24 +0530

    API_Testing

---
+++ Test Suites/API_Testing/API_Testing.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API_Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f7db7951-d2ea-4aaa-b209-3cab70fb8937</testSuiteGuid>
+   <testCaseLink>
+      <guid>8a4999f3-e956-4904-aeee-4a94663ef286</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User_Details_Verification/Details_Verification_DELETE_TC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15f00024-7519-466f-ba32-bc7fa280845a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User_Details_Verification/Details_Verification_GET_TC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c2e97e8-4972-4470-a932-1e13b89433c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User_Details_Verification/Details_Verification_POST_TC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>539816e5-7a3b-4803-9290-b8d0d2cbbaf7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/User_Details_Verification/Details_Verification_PUT_TC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
